db82a420e903713beb2c613bf9b7864dd877e063
kamral <kamral010101@gmail.com>
2020-11-09 20:40:41 +0300

    add django-toolbar

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    # на основе title будем формировать slug
+    prepopulated_fields = {'slug':('title',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    # на основе title будем формировать slug
+    prepopulated_fields = {'slug':('title',)}
+
+class TagAdmin(admin.ModelAdmin):
+    # на основе title будем формировать slug
+    prepopulated_fields = {'slug':('title',)}
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Post,PostAdmin)
+admin.site.register(Tag,TagAdmin)
--- siteblog/urls.py
+++ siteblog/urls.py
@@ -17,12 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
-
-
+import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
+
 ]
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns +=static(settings.MEDIA_URL,
                          document_root=settings.MEDIA_ROOT)
